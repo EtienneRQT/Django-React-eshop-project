315e9eeeb2da74cd0f7da6edd6637d633bb6ca1c
EtienneRQT <etienne.riquet@sfr.fr>
2021-10-01 22:25:45 +0200

    changed login with username to email with signals

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "base",
     "rest_framework",
     "corsheaders",
+    "base.apps.BaseConfig",
 ]
 
 REST_FRAMEWORK = {
