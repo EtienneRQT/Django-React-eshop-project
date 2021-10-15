4ac84aabdbe9947dd4f79a28cce057241febf47e
Etienne <etienne.riquet@sfr.fr>
2021-10-15 20:33:30 +0200

    added user action and reducer and updated the store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 import {
   productListReducer,
   productDetailsReducer,
@@ -10,6 +11,7 @@ import {
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  userLogin: userLoginReducer,
   cart: cartReducer,
 });
 
@@ -17,9 +19,15 @@ const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
-  cart: {cartItems: cartItemsFromStorage}
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
+
 const middleWare = [thunk];
 const store = createStore(
   reducer,
